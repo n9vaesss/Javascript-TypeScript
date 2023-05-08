0c57d45c5adc0f07b8249f541ae83aeace5bd9c5
Caio Novaes <caioo243324333@gmail.com>
2023-05-08 19:45:53 -0300

    inicio exercicio calculadora

---
+++ aula57/assets/js/main.js
@@ -0,0 +1,27 @@
+function criaCalculadora(){
+  return{
+    display: document.querySelector('.display'),
+
+
+    inicia(){
+      this.cliqueBotoes()
+    },
+
+    cliqueBotoes(){
+      //this -> calculadora
+      document.addEventListener('click', function(e){
+        const el = e.target
+        if(el.classList.contains('btn-num')){
+          this.btnParaDisplay()
+        }
+      })
+    },
+
+    btnParaDisplay(){
+
+    }
+  }
+}
+
+const calculadora = criaCalculadora()
+calculadora.inicia()
