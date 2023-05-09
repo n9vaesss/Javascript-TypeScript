4c2b23373136e373c47076e1f526f822dbc05674
Caio Novaes <caioo243324333@gmail.com>
2023-05-09 19:57:47 -0300

    final aula57 09-05 19:57

--- aula57/assets/js/main.js
+++ aula57/assets/js/main.js
@@ -1,24 +1,69 @@
-function criaCalculadora(){
-  return{
+function criaCalculadora() {
+  return {
     display: document.querySelector('.display'),
 
-
-    inicia(){
+    inicia() {
       this.cliqueBotoes()
+      this.precionaEnter()
     },
 
-    cliqueBotoes(){
-      //this -> calculadora
-      document.addEventListener('click', function(e){
-        const el = e.target
-        if(el.classList.contains('btn-num')){
-          this.btnParaDisplay()
+    pressionaEnter(){
+      this.display.addEventListener('keyup', (e) =>{
+        if(e.keyCode == 13){
+          this.realizaConta()
         }
       })
     },
 
-    btnParaDisplay(){
+    clearDisplay() {
+      this.display.value = ''
+    },
 
+    apagaUM() {
+      this.display.value = this.display.value.slice(0, -1)
+    },
+
+    realizaConta() {
+      let conta = this.display.value
+
+      try {
+        conta = eval(conta)
+
+        if (!conta) {
+          alert("Conta invalida")
+          return
+        }
+
+        this.display.value = conta
+      } catch (e) {
+        alert("Conta invalida")
+      }
+    },
+
+    cliqueBotoes() {
+      //this -> calculadora
+      document.addEventListener('click', e => {
+        const el = e.target
+        if (el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText)
+        }
+
+        if (el.classList.contains('btn-clear')) {
+          this.clearDisplay()
+        }
+
+        if (el.classList.contains('btn-del')) {
+          this.apagaUM()
+        }
+
+        if (el.classList.contains('btn-eq')) {
+          this.realizaConta()
+        }
+      })
+    },
+
+    btnParaDisplay(valor) {
+      this.display.value += valor
     }
   }
 }
