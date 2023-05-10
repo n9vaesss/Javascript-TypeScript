8c9da3a0e50fd41592d5d4eec08200ceba77a6f8
Caio Novaes <caioo243324333@gmail.com>
2023-05-10 10:06:34 -0300

    aula58 10-05 10:06

---
+++ aula58/aula58.js
@@ -0,0 +1,15 @@
+function Pessoa(nome, sobrenome) {
+    const ID = 123456
+
+    this.nome = nome
+    this.sobrenome = sobrenome
+
+    this.metodo = () =>{
+        console.log('Sou um metodo')
+    }
+}
+
+const p1 = new Pessoa('Luiz', 'Otavio')
+const p2 = new Pessoa('Caio', 'Novaes')
+
+console.log(p1.nome)
